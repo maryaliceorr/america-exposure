d42a9297526ef65396ce45ecbba23e53c9d10337
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-17 21:20:20 -0500

    wip commit

---
+++ src/components/Regions/Regions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Regions.css';
+
+class Regions extends React.Component {
+  render () {
+    return (
+      <div className="Regions">
+        <h2>Regions</h2>
+      </div>
+    );
+  }
+}
+
+export default Regions;
