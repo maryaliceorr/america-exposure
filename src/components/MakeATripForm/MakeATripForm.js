ac3296a244888d5ece2bc8b2b6864774043f27f8
Mary Alice Orr <m.a.orr17@gmail.com>
2018-07-31 21:24:38 -0500

    got rid of link tag

--- src/components/MakeATripForm/MakeATripForm.js
+++ src/components/MakeATripForm/MakeATripForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import tripRequests from '../../firebaseCalls/trip';
 import authRequests from '../../firebaseCalls/auth';
