74e6352d5f7226a1c86c4562eec9963f707e4149
Mary Alice Orr <m.a.orr17@gmail.com>
2018-07-31 23:32:10 -0500

    wip commit

--- src/pages/BucketListPage/BucketListPage.js
+++ src/pages/BucketListPage/BucketListPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import bucketListRequests from '../../firebaseCalls/bucketList';
 import authRequests from '../../firebaseCalls/auth';
+import spotRequests from '../../firebaseCalls/spots';
 
 import './BucketListPage.css';
 
@@ -21,26 +22,55 @@ state = {
       .catch((error) => {
         console.error('error with getBucketSpots request', error);
       });
+    spotRequests
+      .getSpots()
+      .then((spots) => {
+        this.setState({spots});
+      })
+      .catch((error) => {
+        console.error('error with getSpots', error);
+      });
   };
 
   render () {
-    const bucketSpots = this.state.bucketSpots.map((bucketSpot) => {
-      // const imageUrl = require(`${bucketSpot.image}`);
-      return (
+    const {bucketSpots, spots} = this.state;
+    const getSpots = bucketSpots.map((bucketSpot) => {
+      const spot = spots.find(x => x.id === bucketSpot.spotId)
+       if (spot)  {return (
         <div key={bucketSpot.id}>
           <h2>{bucketSpot.locationName}</h2>
           <img src={bucketSpot.image} alt={bucketSpot.locationName}/>
           <h2>Hello</h2>
-        </div>
-      )
-    });
+         </div>
+        )}
+        return '';
+    })
     return (
-      <div className="CameraNav">
-        <h1>Bucket List</h1>
-        {bucketSpots}
+      <div className="BucketListPage">
+        {getSpots}
       </div>
     );
   }
 }
+
+
+//     const bucketSpots = this.state.bucketSpots.map((bucketSpot) => {
+//       // const imageUrl = require(`${bucketSpot.image}`);
+//       return (
+//         <div key={bucketSpot.id}>
+//           <h2>{bucketSpot.locationName}</h2>
+//           <img src={bucketSpot.image} alt={bucketSpot.locationName}/>
+//           <h2>Hello</h2>
+//         </div>
+//       )
+//     });
+//     return (
+//       <div className="BucketList">
+//         <h1>Bucket List</h1>
+//         {bucketSpots}
+//       </div>
+//     );
+//   }
+// }
 
 export default BucketListPage;
