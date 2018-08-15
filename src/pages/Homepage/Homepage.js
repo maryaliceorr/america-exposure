5cc3306f85da9628ae2f05cb7d0fce79d16a70ca
Mary Alice Orr <m.a.orr17@gmail.com>
2018-08-14 21:24:31 -0500

    last styles

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -7,11 +7,11 @@ class Homepage extends React.Component {
   render () {
     return (
       <div className="Homepage">
-      <div className="top-space text-center">
-       <img className="homepage-logo text-center" src={require("./images/logo.png")} alt="america exposure logo"/>
+        <div className="top-space text-center">
+          <img className="homepage-logo text-center" src={require("./images/logo.png")} alt="america exposure logo"/>
+          <p className="mountains col-md-8 col-md-offset-2">"The mountains are calling and I must go." -John Muir</p>
         </div>
-        {/* <h2 className="text-center">CHOOSE A CATEGORY</h2> */}
-        <CameraNav />
+      <CameraNav />
       </div>
     );
   }
